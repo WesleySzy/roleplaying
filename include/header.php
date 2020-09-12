dd1ff75e039758e52de8215905573cd118ad9970
Pedro L <42793973+akadarin@users.noreply.github.com>
2020-09-12 19:16:23 -0300

    WERLE VC NAO SABE MEXE COM DIV
    DESISTE

--- include/header.php
+++ include/header.php
@@ -1,25 +1,30 @@
 <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand text-primary" href="<?php $urlProjeto?>">____<i class="fab fa-accessible-icon fa-4x"></i></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" borderhref="#">
+            <img src="https://scontent.fldb1-1.fna.fbcdn.net/v/t1.0-9/118765451_107382697764738_1455136094544867385_n.jpg?_nc_cat=1&_nc_sid=09cbfe&_nc_ohc=lDx5qF1RvBsAX9gqOoO&_nc_ht=scontent.fldb1-1.fna&oh=ce7f61217c9bc465a0f3d3cff48c071d&oe=5F7991C0" width="30" height="30" alt="">
+        </a>
+        <a class="navbar-brand" borderhref="#">
+            <img src="https://i.pinimg.com/280x280_RS/e6/bc/4f/e6bc4fc3b925dd6fbe7d704a0f5d3b3b.jpg" width="30" height="30" alt="">
+        </a>
+        <a class="navbar-brand" href="#">Super Xandao</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
+            aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
-            <ul class="navbar-nav">
+
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                 <li class="nav-item active">
-                    <a class="nav-link" href="<?php $urlProjeto?>home" <?php if ($paginaAtual == 1){echo "active";}?>>Pagina Inicial</a>
+                    <a class="nav-link" href="#">Perfil <span class="sr-only">(current)</span></a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php $urlProjeto?>perfil" <?php if ($paginaAtual == 1){echo "active";}?>>Perfil</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php $urlProjeto?>configuracoes" <?php if ($paginaAtual == 1){echo "active";}?>>Configuracoes</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="logout.php">Sair</a>
+                <li class="nav-item active">
+                    <a class="nav-link" href="logout.php">Sair</span></a>
                 </li>
             </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+            </form>
         </div>
     </nav>
 </header>
